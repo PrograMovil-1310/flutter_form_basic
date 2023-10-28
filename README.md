25a938efc0b7574b5a19854206a0391db63b2966
Ivan Cruz <ivancruzhn@gmail.com>
2023-10-28 10:28:50 -0600

    input number, radio boton y lista desplegable

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Proyecto base para mostrar el uso del Widget Form y algunos Field
 
-1. TextFormFiel
+1. TextFormField
--- lib/src/form.dart
+++ lib/src/form.dart
@@ -7,7 +7,21 @@ class FormBasico extends StatefulWidget {
 }
 
 class _FormBasico extends State<FormBasico> {
-  String nombreUsuario = "Prueba";
+  String nombreUsuario = "";
+  String apellidoUsuario = "";
+  int edadUsuario = 0;
+  bool sexoUsuario = false;
+
+  final List<DropdownMenuEntry<String>> _listPais = [
+    DropdownMenuEntry<String>(
+      label: "Estados Unidos",
+      value: 'EEUU',
+    ),
+    DropdownMenuEntry<String>(
+      label: "Honduras",
+      value: 'HN',
+    ),
+  ];
   @override
   build(BuildContext context) {
     return Scaffold(
@@ -24,13 +38,72 @@ class _FormBasico extends State<FormBasico> {
                           icon: const Icon(Icons.person_outline),
                           suffixIcon: const Icon(Icons.send),
                           border: const OutlineInputBorder(),
-                          helperText: 'Mostrar los mensajes de advertencia',
                           counterText: '${nombreUsuario.length} caracteres'),
                       onChanged: (input) =>
                           setState(() => nombreUsuario = input),
                     ),
                     const Divider(),
-                    Text(nombreUsuario),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Ingrese su apellido",
+                          icon: const Icon(Icons.person_outline),
+                          suffixIcon: const Icon(Icons.send),
+                          border: const OutlineInputBorder(),
+                          counterText: '${apellidoUsuario.length} caracteres'),
+                      onChanged: (input) =>
+                          setState(() => apellidoUsuario = input),
+                    ),
+                    const Divider(),
+                    TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Ingrese su edad",
+                          icon: Icon(Icons.person_outline),
+                          suffixIcon: Icon(Icons.numbers),
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        onChanged: (input) => setState(
+                              () => edadUsuario = int.parse(input),
+                            )),
+                    const Divider(),
+                    Row(
+                      children: [
+                        const Text("Sexo: "),
+                        Radio(
+                            toggleable: true,
+                            value: 0,
+                            groupValue: edadUsuario,
+                            onChanged: (input) =>
+                                setState(() => input == 0 ? false : true)),
+                        const Text("Mujer"),
+                        Radio(
+                            toggleable: true,
+                            value: 1,
+                            groupValue: edadUsuario,
+                            onChanged: (input) => setState(
+                                () => sexoUsuario = input == 0 ? false : true)),
+                        const Text("Hombre")
+                      ],
+                    ),
+                    const Divider(),
+                    Row(children: [
+                      const Text("Nacionalidad: "),
+                      DropdownMenu(dropdownMenuEntries: _listPais),
+                    ]),
+                    const Divider(),
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      FloatingActionButton(
+                          child: const Icon(Icons.save),
+                          onPressed: () => {
+                                print(
+                                    "${nombreUsuario} ${apellidoUsuario} ${edadUsuario}")
+                              })
+                    ]),
+                    Divider(),
+                    Text("${nombreUsuario} ${apellidoUsuario} ${edadUsuario}")
                   ])),
         ));
   }
